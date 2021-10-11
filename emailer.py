f762d44f7c0043ee33194f372f1b6f663e42dd72
seriouswill <73595384+seriouswill@users.noreply.github.com>
2021-10-11 09:28:50 +0100

    added the scraper functionality into the schedule
    the scraper functionality was only pulling once when i left the program running, so now its runs on a schedule two minutes before the email schedule!

--- emailer.py
+++ emailer.py
@@ -8,11 +8,52 @@ from email.mime.multipart import MIMEMultipart
 import smtplib
 import os
 
+import ssl
+import smtplib
+import urllib
+from bs4 import BeautifulSoup
 
-from webscraper import text
+
+def scrape():
+
+    url = "https://www.jftna.org/jft/index.php"
+
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        soup = BeautifulSoup(html)
+
+    # kill all script and style elements
+    for script in soup(["script", "style", "a", "<div id=\"bottom\" >", "title", "meta", "htmlhead", "link", "head"]):
+        script.extract()    # rip it out
+
+    for tags in soup(["td", "h2", "table", "tr", "body", "tr", "h1", "br", "i", "b", "tbody", "html"]):
+        tags.unwrap()
+    text = soup
+
+    # # break into lines and remove leading and trailing space on each
+    # lines = (line.strip() for line in text.splitlines())
+    # # break multi-headlines into a line each
+    # chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # # drop blank lines
+    # text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    # havent got these formatting elements quite right yet
+
+    td_tag = soup.td
+    h2_tag = soup.h2
+
+    return text
+
+    # fo = open('foo.txt', 'w')
+    # fo.seek(0, 2)
+    # line = fo.writelines(text)
+    # fo.close()
+    # writing done :)
+print(scrape())
 
 
 def mail():
+    global text
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
     smtp.ehlo()
     smtp.starttls()
@@ -20,16 +61,18 @@ def mail():
 
     msg = MIMEMultipart()
     msg['Subject'] = "Just For Today"
-    msg.attach(MIMEText(text))
+    msg.attach(MIMEText(scrape))
 
-    to = ["williamjamesoc@gmail.com"]
+    to = ["williamjamesoc@gmail.com", "thelise.rocquin@gmail.com"]
     smtp.sendmail(from_addr="justfortodaytester@gmail.com",
                   to_addrs=to, msg=msg.as_string())
     print("Sending...")
     smtp.quit()
 
 
-schedule.every().day.at("11:13").do(mail)
+schedule.every().day.at("09:28").do(scrape)
+
+schedule.every().day.at("09:30").do(mail)
 
 while True:
     schedule.run_pending()
