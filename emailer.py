6b32c4a2c9f5c5c9580327b0b74eeb9de2a00917
seriouswill <73595384+seriouswill@users.noreply.github.com>
2021-10-18 09:55:12 +0100

    cleaned up formatting, finalised scraper functionality
    also added my apprenticeship write up

--- emailer.py
+++ emailer.py
@@ -1,59 +1,16 @@
 import schedule
 import time
 
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText                # for text in email
+from email.mime.image import MIMEImage              # for images in email
 from email.mime.multipart import MIMEMultipart
 import smtplib
-import os
 
-import ssl
-import smtplib
-import urllib
-from bs4 import BeautifulSoup
-
-
-def scrape():
-
-    url = "https://www.jftna.org/jft/index.php"
-
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        soup = BeautifulSoup(html)
-
-    # kill all script and style elements
-    for script in soup(["script", "style", "a", "<div id=\"bottom\" >", "title", "meta", "htmlhead", "link", "head"]):
-        script.extract()    # rip it out
-
-    for tags in soup(["td", "h2", "table", "tr", "body", "tr", "h1", "br", "i", "b", "tbody", "html"]):
-        tags.unwrap()
-    text = soup
-
-    # # break into lines and remove leading and trailing space on each
-    # lines = (line.strip() for line in text.splitlines())
-    # # break multi-headlines into a line each
-    # chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    # # drop blank lines
-    # text = '\n'.join(chunk for chunk in chunks if chunk)
-
-    # havent got these formatting elements quite right yet
-
-    td_tag = soup.td
-    h2_tag = soup.h2
-
-    return text
-
-    # fo = open('foo.txt', 'w')
-    # fo.seek(0, 2)
-    # line = fo.writelines(text)
-    # fo.close()
-    # writing done :)
-print(scrape())
+from webscraper import scrape
 
 
 def mail():
-    global text
+
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
     smtp.ehlo()
     smtp.starttls()
@@ -61,18 +18,17 @@ def mail():
 
     msg = MIMEMultipart()
     msg['Subject'] = "Just For Today"
-    msg.attach(MIMEText(scrape))
-
-    to = ["williamjamesoc@gmail.com", "thelise.rocquin@gmail.com"]
+    msg.attach(MIMEText(scrape()))
+    to = ["williamjamesoc@gmail.com", "josefsquire@gmail.com"]
     smtp.sendmail(from_addr="justfortodaytester@gmail.com",
                   to_addrs=to, msg=msg.as_string())
     print("Sending...")
     smtp.quit()
 
 
-schedule.every().day.at("09:28").do(scrape)
-
+schedule.every().day.at("09:30").do(scrape)
 schedule.every().day.at("09:30").do(mail)
+
 
 while True:
     schedule.run_pending()
--- webscraper.py
+++ webscraper.py
@@ -1,54 +1,31 @@
-import ssl
-import smtplib
 import urllib
 from bs4 import BeautifulSoup
 url = "https://www.jftna.org/jft/index.php"
 
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    soup = BeautifulSoup(html)
+def scrape():
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        soup = BeautifulSoup(html)
 
-# kill all script and style elements
-for script in soup(["script", "style", "a", "<div id=\"bottom\" >", "title", "meta", "htmlhead", "link", "head"]):
-    script.extract()    # rip it out
+    for script in soup(["script", "style", "a", "<div id=\"bottom\" >", "title", "meta", "htmlhead", "link", "head"]):
+        script.extract()    # This rips out the tags and data inside the tags from the object
 
-for tags in soup(["td", "h2", "table", "tr", "body", "tr", "h1", "br", "i", "b", "tbody", "html"]):
-    tags.unwrap()
-text = soup
-# get text
-#text = soup.select('.C_doc')[0].get_text()
-#text = soup.select('.content')[0].get_text()
+    for tags in soup(["td", "h2", "table", "tr", "body", "tr", "h1", "br", "i", "b", "tbody", "html"]):
+        # This removes the tags from around the data inside, leaving just the text without tags.
+        tags.unwrap()
 
-# if soup.select('td'):
-#     text = soup.select('td')[0].get_text()
-# else:
-#     text = soup.select('i')[0].get_text()
+    text = str(soup)
 
-# # break into lines and remove leading and trailing space on each
-# lines = (line.strip() for line in text.splitlines())
-# # break multi-headlines into a line each
-# chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# # drop blank lines
-# text = '\n'.join(chunk for chunk in chunks if chunk)
+    # # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    print("Scraping...")
+    return text
 
 
-td_tag = soup.td
-h2_tag = soup.h2
-# td_tag.unwrap()
-# h2_tag.unwrap()
-
-
-# for text_x in text:
-#     print(text_x)
-
-# text = text.strip()
-
-print(text)
-
-
-# fo = open('foo.txt', 'w')
-# fo.seek(0, 2)
-# line = fo.writelines(text)
-# fo.close()
-# writing done :)
+scrape()
